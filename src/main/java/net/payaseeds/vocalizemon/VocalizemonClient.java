146ac06fc97551fb7fbcb6ce7725b37822c19c6e
Ayoung588 <163087733+Ayoung588@users.noreply.github.com>
2025-03-28 14:28:09 -0700

    Initial commit

---
+++ src/main/java/net/payaseeds/vocalizemon/VocalizemonClient.java
@@ -0,0 +1,10 @@
+package net.payaseeds.vocalizemon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VocalizemonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/payaseeds/vocalizemon/VocalizemonDataGenerator.java
@@ -0,0 +1,11 @@
+package net.payaseeds.vocalizemon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VocalizemonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
