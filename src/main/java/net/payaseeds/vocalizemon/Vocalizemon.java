da893a73bea49d16868fbfa4511cbac9a207f16a
Ayoung588 <163087733+Ayoung588@users.noreply.github.com>
2025-03-29 19:34:30 -0700

    stuff

--- src/main/java/net/payaseeds/vocalizemon/Vocalizemon.java
+++ src/main/java/net/payaseeds/vocalizemon/Vocalizemon.java
@@ -1,24 +1,22 @@
 package net.payaseeds.vocalizemon;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Vocalizemon implements ModInitializer {
 	public static final String MOD_ID = "vocalizemon";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Vocalizemon Initialized!");
 
-		LOGGER.info("Hello Fabric world!");
+		try {
+			Class<?> cobblemonClass = Class.forName("net.cobblemon.Cobblemon");
+			LOGGER.info("Cobblemon class found: " + cobblemonClass.getName());
+		} catch (ClassNotFoundException e) {
+			LOGGER.error("Cobblemon class not found!", e);
+		}
 	}
 }
